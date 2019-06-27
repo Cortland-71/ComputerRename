d3371425c9cce87db300d9b5512cf214e631bad6
XB71 <32171659+XB71@users.noreply.github.com>
2019-06-27 14:00:30 -0700

    Computer renamer

---
+++ RenameComputers.ps1
@@ -0,0 +1,11 @@
+﻿$computers = Import-Csv "data" -Header New,  Old | Select-Object "New", "Old"
+
+$cred = Get-Credential 
+
+foreach ($computer in $computers)
+{
+    Rename-Computer -ComputerName $computer.Old -NewName $computer.New -DomainCredential ($cred) -Force
+}
+
+
+
